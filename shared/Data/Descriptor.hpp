d0dd578fff82c618c99a68d87fe7d076d2b2ddfb
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-04-16 20:44:58 -0400

    Improve allocations and copying

--- shared/Data/Descriptor.hpp
+++ shared/Data/Descriptor.hpp
@@ -13,28 +13,28 @@ namespace Qosmetics::Core
         Descriptor(const Descriptor& other);
         Descriptor(const rapidjson::Value& val, std::string_view filePath);
         Descriptor(const rapidjson::Value& val);
-        Descriptor(std::string author, std::string name, std::string description, std::string filePath, std::string coverImage) : author(author), name(name), description(description), filePath(filePath), coverImage(coverImage){};
+        Descriptor(std::string_view author, std::string_view name, std::string_view description, std::string_view filePath, std::string_view coverImage) : author(author), name(name), description(description), filePath(filePath), coverImage(coverImage){};
         rapidjson::Value ToJson(rapidjson::Document::AllocatorType& allocator) const;
 
-        std::string_view get_author() const;
-        std::string_view get_name() const;
-        std::string_view get_description() const;
-        std::string_view get_filePath() const;
-        std::string_view get_coverImage() const;
+        [[nodiscard]] std::string_view get_author() const;
+        [[nodiscard]] std::string_view get_name() const;
+        [[nodiscard]] std::string_view get_description() const;
+        [[nodiscard]] std::string_view get_filePath() const;
+        [[nodiscard]] std::string_view get_coverImage() const;
 
     private:
-        std::string author = "";
-        std::string name = "";
-        std::string description = "";
-        std::string filePath = "";
-        std::string coverImage = "";
+        std::string author;
+        std::string name;
+        std::string description;
+        std::string filePath;
+        std::string coverImage;
 
         friend DescriptorComparator;
     };
 
     struct DescriptorComparator
     {
-        bool operator()(const Descriptor& lhs, const Descriptor& rhs) const
+        constexpr bool operator()(const Descriptor& lhs, const Descriptor& rhs) const
         {
             return lhs.name < rhs.name;
         }
