0e8d864e55f39372f1d9578c42f5327e70c334ab
NgbatzYT <Ngbatman13@outlook.com>
2026-01-08 11:18:15 +1100

    Somehow this completely fixes Qosmetics for 1.37.0
    Don querstion it

---
+++ src/Utils/DummyUtils.cpp
@@ -0,0 +1,15 @@
+// Dont question how it works just know it works
+extern "C" {
+void _ZNSt6__ndk119__shared_mutex_base11lock_sharedEv() {
+}
+
+void _ZNSt6__ndk119__shared_mutex_base13unlock_sharedEv() {
+}
+
+void _ZNSt6__ndk119__shared_mutex_base4lockEv() {
+}
+
+void _ZNSt6__ndk119__shared_mutex_base6unlockEv() {
+}
+
+}
